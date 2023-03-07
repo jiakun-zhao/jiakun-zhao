31a230ed52dbadd40c84df9d30b74f0a7524b0d4
Jiakun Zhao <jiakun.zhao@outlook.com>
2023-03-07 18:07:03 +0800

    chore: refactor backup

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
 import Pages from 'vite-plugin-pages'
 import UnoCSS from 'unocss/vite'
+import ImportCDN from '@jiakun-zhao/vite-plugin-cdn'
 
 import { UnoConfig, VitePlugins } from './use'
 
@@ -13,6 +14,15 @@ export default defineConfig({
         Pages({ dirs: [{ dir: 'src/pages', baseRoute: '' }] }),
         UnoCSS(UnoConfig),
         VitePlugins(),
+        ImportCDN({
+            url: 'https://welostyou.host/npm/',
+            modules: [
+                { name: 'vue', var: 'Vue', path: 'dist/vue.runtime.global.prod.js' },
+                { name: 'vue-router', var: 'VueRouter', path: 'dist/vue-router.global.prod.js' },
+                { name: '@vueuse/core', var: 'VueUse', path: 'index.iife.min.js' },
+                { name: '@vueuse/shared', var: 'VueUse', path: 'index.iife.min.js' },
+            ],
+        }),
     ],
     resolve: {
         alias: { '~': fileURLToPath(new URL('./src', import.meta.url)) },
