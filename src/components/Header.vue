489f1fc7c79e42cb8b8e323816d98838c48d7a2c
Jiakun Zhao <jiakun.zhao@outlook.com>
2023-03-16 06:54:04 +0800

    chore: header content

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
     <h1 h>Jiakun Zhao</h1>
-    <p my-4 mt-12>Designer / Developer</p>
+    <p my-4 mt-12>Front-end developer / Graphic designer</p>
     <p my-4>Currently working on WeChat applet development.</p>
     <p my-4>Struggling to learn English...😢</p>
 </template>
