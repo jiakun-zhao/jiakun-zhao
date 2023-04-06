bad42c1956a687e1d63dd1563a2d30cabb7b9764
Jiakun Zhao <jiakun.zhao@outlook.com>
2023-04-07 01:48:35 +0800

    chore: html minify

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,37 @@
 import { defineConfig } from 'astro/config'
 import { visit } from 'unist-util-visit'
+import { fileURLToPath } from 'node:url'
+import { minify } from 'html-minifier'
+import glob from 'fast-glob'
+import { readFile, writeFile } from 'node:fs/promises'
 
 export default defineConfig({
+    site: 'https://zhaojiakun.cn',
     server: {
         host: true,
     },
+    integrations: [
+        {
+            name: 'astro-html-minifier',
+            hooks: {
+                'astro:build:done': async ({ dir }) => {
+                    const path = fileURLToPath(dir)
+                    const files = await glob(`${path}/**/*.html`)
+                    for(const filePath of files) {
+                        const html = await readFile(filePath, 'utf-8')
+                        const minified = minify(html, {
+                            removeComments: true,
+                            removeAttributeQuotes: true,
+                            collapseWhitespace: true,
+                            minifyJS: true,
+                            minifyCSS: true,
+                        })
+                        await writeFile(filePath, minified)
+                    }
+                },
+            },
+        },
+    ],
     markdown: {
         drafts: true,
         remarkPlugins: [
